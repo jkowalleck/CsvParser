908ff8a9c74c84886a93843ca15c6ba62aa4479e
Jan Kowalleck <jan.kowalleck@gfk.com>
2013-11-26 01:16:41 +0100

    huge rewrite, restructure, ... ... prepare for unit test

--- CsvParser.js
+++ CsvParser.js
@@ -22,71 +22,179 @@ CsvParser.prototype = {
 		this.data = [];
 	} ,
 	
-	lineSplitRE : /\r?\n/ , 
+	startsOpenField : function (string)
+	{
+		if ( ! string.length ) 
+		{	
+			return false;
+		}
+		
+		return ( string[0] == this.enclose );
+	} ,
+	endsOpenField : function (string) 
+	{
+		var string_length = string.length;
+		if ( ! string_length ) 
+		{
+			return false; 
+		} 
+		
+		var endsEnclose = ( string[string_length -1] == this.enclose );
+		if ( !endsEnclose ) 
+		{
+			return false;
+		}
+		
+		var escape = this.escape;
+		var escaped = false;
+		for ( var c=string_length-1 , charBefore1,charBefore2 ; c >= 0 ; )
+		{
+			charBefore1 = string[--c];
+			if ( charBefore1 != escape )
+			{
+				break;
+			}
+			charBefore2 = string[--c];
+			if ( charBefore2 != escape )
+			{
+				escaped = true;
+				break;
+			}
+		}
+		if ( escaped )
+		{
+			return false;
+		}		
+		
+		return true;
+	} ,
+	isOpenField : function (string)
+	{
+		return this.startsOpenField(string) && ( string.length==1 || !this.endsOpenField(string) );
+	} ,	
+	/* untested >> splitTrailingOpenFiled : function (lines)
+	{
+		var line_length = lines.length;
+		if ( ! line_length )
+		{
+			return false;
+		}
+		var lastLine = lines[line_length -1];
+		
+		var lastLine_length = lastLine.length;
+		if ( ! lastLine_length )
+		{
+			return false;
+		}
+		var lastField = lastLine[lastLine_length -1];
+		
+		var isOpenFiled = this.isOpenField(lastField);
+		if  ( ! isOpenFiled )
+		{
+			return false;
+		}
+		
+		return lastLine.pop();	
+	} , */
+	
+	trimOpenFiled : function (string , left, right) 
+	{
+		if ( left && right )		
+			return string.slice(1, -1);
+		
+		if ( left ) 
+			return string.slice(1);
+
+		if ( right ) 
+			return string.slice(0, -1);
+
+		return string;
+	} ,
+	
+	clearEscape : function (string) 
+	{
+		return string.replace(new RegExp("\\"+this.escape+"(.)","g"),"$1");
+	} ,
+	
+	lineSplitRE : /\r?\n/ ,
+	splitLines : function (string)
+	{	
+		return string.split(this.lineSplitRE);
+	} ,
 	
 	parse : function (string)
 	{
-		var lines = string.split(this.lineSplitRE);
-		this.parseLines(lines);
+		var trailingOpenLine = this.parseLines(this.splitLines(string));
+		if ( trailingOpenLine ) 
+		{
+			this.data.push(trailingOpenLine);
+		}
 	} ,
-	
 	parseLines : function (lines)
 	{
+		// algorithm totally unoptimized so the job is clear
 		this.reset();
-	
-		var escape = this.escape, escapeRE = new RegExp(escape+"[^"+escape+"]","g")
-		  , delimiter = this.delimiter 
-		  , enclose = this.enclose
-		  ; 
 		
-		var split, lineData=[], lineField , openField=false , lineField_length;
-		for ( var l=0, ll=lines.length; l<ll ; ++l )
+		var data = [];
+	
+		var delimiter = this.delimiter;
+		
+		var openField=false;
+		for ( var l=0, ll=lines.length , line,split; l<ll ; ++l )
 		{
-			split = lines[l].split(delimiter);
-			for ( var s=0, sl=split.length ; s < sl ; ++s )
+			line = lines[l];
+			if ( openField )
+			{
+				if ( l > 0 )
+				{
+					openField += "\n";
+				}
+			}
+			
+			split = line.split(delimiter);
+			for ( var s=0, sl=split.length , lineField,lineData=(openField?data.pop():[]) ; s<sl ; ++s )
 			{
 				lineField = split[s];
 				
 				if ( openField )
 				{
-					if ( s > 0 ) 
+					if ( s > 0 )
 					{
 						openField += delimiter;
-					}
-					lineField = openField + lineField;
-					openField = true;
+					}					
+					openField += lineField;
+				}
+				else if ( this.startsOpenField(lineField) )
+				{
+					openField = lineField = this.trimOpenFiled(lineField, true, false);
 				}				
 				
-				if ( openField || lineField[0] == enclose ) 
-				{
-					openField = false;
-					lineField_length = lineField.length;
-					if ( lineField[lineField_length-1] != enclose || lineField[lineField_length-2] == escape )
+				if ( openField )
+				{ 
+					if ( ! this.endsOpenField(openField) )
 					{
-						openField = lineField;
 						continue;
 					}
-					lineField = lineField.slice(1, -1);
-				}
-				
-				lineData.push(lineField.replace(escapeRE,""));
-			}
 
-			if ( openField )
-			{
-				openField += "\n";
+					lineField = this.trimOpenFiled(openField, false, true);
+					openField = false;
+				}			
+
+				lineData.push(this.clearEscape(lineField));
 			}
-			else
-			{			
-				this.data.push(lineData);
-				lineData = [];
-			}
+			
+			data.push(lineData);
 		}
+		
+		this.data = data;
 		
 		if ( openField )
 		{
-			lineData.push(openField);
-			this.data.push(lineData);
-		}
+			var trailingOpenLine = data.pop();
+			trailingOpenLine.push(this.clearEscape(openField));
+			return trailingOpenLine;
+		} 
+		
+		return false;
 	}
 };
